a32e82c721d39914b2c92474022bf643748e11b8
KunE <entksentks10@naver.com>
2021-02-11 13:11:48 +0900

    v1.0.1
    ★ | 신규 C 등급 능력 쥐
      - 플레이어를 물어 스턴과 데미지를 추가로 입혀보세요 !
    
     ★ | 신규 L 등급 능력 메이릴
       - 숲의 여신 메이릴,
       - 페어리들과 함께 전장에서 싸워나가보세요 !
    
     ★ | 신규 S 등급 마루스
       - 도박에 검사 마루스,
       - 랜덤으로 추가 데미지를 뽑아 강력한 데미지를 줘보세요 !
    
     ★ | 에드온 버전 관전 오타 수정
     ★ | aw addon 에서 표기되는 능력 개수 오타수정
     ★ | 능력 마루스 짜잘한 버그와 각종 문구 수정

--- src/main/java/KunEAbility/Mars.java
+++ src/main/java/KunEAbility/Mars.java
@@ -89,7 +89,7 @@ public class Mars extends AbilityBase implements ActiveHandler {
             if (!cooldown.isCooldown()) {
                 this.thisdamage = random.nextInt(max_random_damage + 1);
                 getPlayer().sendMessage(prefix + "랜덤으로 뽑은 숫자 : §c" + thisdamage);
-                getPlayer().sendMessage(prefix + "당신은 이제부터" + thisdamage + " 의 데미지를 추가로 공격합니다.");
+                getPlayer().sendMessage(prefix + "당신은 이제부터"  + thisdamage + "의 데미지를 추가로 공격합니다.");
                 SoundLib.UI_TOAST_CHALLENGE_COMPLETE.playSound(getPlayer());
                 actionbarChannel.update("§7현재 추가 데미지 §f: " + thisdamage);
                 getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(max_health);
